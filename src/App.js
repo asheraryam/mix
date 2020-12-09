8a9ff21c701644f3a93cae0fa2f45e02cc706317
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-12-09 21:25:53 +0000

    :sparkles: Move functions into app scope

--- src/App.js
+++ src/App.js
@@ -3,83 +3,88 @@ import './App.css'
 import { blendThings } from './blend'
 import { Container, ColorBlock} from './App.style'
 
-const colors = [
-  "#FF595E",
-  "#FFCA3A" ,
-  "#8AC926",
-  "#1982C4",
-  "#6A4C93"
-]
-
-
-function generateCombos(numberOfColors) {
-  let results = [];
-
-  for (let i = 0; i < numberOfColors - 1; i++) {
-    for (let j = i + 1; j < numberOfColors; j++) {
-      results.push({one: colors[i], two: colors[j]});
-    }
-  }
-
-  return results;
-}
-
-function drawRawColors(numberOfColors) {
-  const blocks = [];
-
+function generateRandomColorsArray(numberOfColors) {
+  const colors = [];
   for (let i = 0; i < numberOfColors; i++) {
-    blocks.push(<ColorBlock key={`rawColor-${i}`} color={colors[i]} />)
+    colors.push(Math.floor(Math.random()*16777215).toString(16))
   }
 
-  return blocks;
+  return colors;
 }
-
-function drawMixedBlocks(numberOfColors) {
-  const combos = generateCombos(numberOfColors);
-  const blocks = [];
-
-  combos.forEach(combo => {
-    for (let i = 1; i <= 100; i++) {
-      blocks.push(<ColorBlock key={`color-${combo.one}-${combo.two}-${i}`} color={blendThings(combo.one, combo.two, i)} />)
-    }
-  })
-
-  return blocks;
-}
-
-
-  // return combos.forEach(combo => {
-  //   for (let i = 1; i <= 100; i++) {
-  //     return <ColorBlock key={`color-${i}`} color={blendThings(combo.one, combo.two, i)} />
-  //   }
-  // })
-
-  // return combos.reduce((pv, cv) => { 
-  //   const arr = []; 
-    
-  //   for (let i = 0; i <= 100; i++) { 
-  //     arr.push(<ColorBlock key={`color-${i}`} color={blendThings(cv.one, cv.two, i)} />); 
-  //   } 
-    
-  //   return [...pv, ...arr]; }, 
-  //   []);
-
-
-
 
 function App() {
-  const [numberOfColors, setNumberOfColors] = useState(2);
+  const initialColors = [
+    "#FF595E",
+    "#FFCA3A" ,
+    "#8AC926",
+    "#1982C4",
+    "#6A4C93"
+  ]
+
+  const [numberOfColors, setNumberOfColors] = useState(5);
+  const [colorsArray, setColorsArray] = useState(initialColors);
+
+  console.log(colorsArray);
+
+  function generateCombos() {
+    let results = [];
+
+    for (let i = 0; i < numberOfColors - 1; i++) {
+      for (let j = i + 1; j < numberOfColors; j++) {
+        results.push({one: colorsArray[i], two: colorsArray[j]});
+      }
+    }
+
+    return results;
+  }
+
+  function drawRawBlocks() {
+    console.log('DRAWING RAW COLORS');
+    const blocks = [];
+  
+    for (let i = 0; i < numberOfColors; i++) {
+      blocks.push(<ColorBlock key={`rawColor-${i}`} color={colorsArray[i]} />)
+    }
+  
+    return blocks;
+  }
+
+  function drawMixedBlocks() {
+    const combos = generateCombos(numberOfColors, colorsArray);
+    const blocks = [];
+  
+    combos.forEach(combo => {
+      for (let i = 1; i <= 100; i++) {
+        blocks.push(<ColorBlock key={`color-${combo.one}-${combo.two}-${i}`} color={blendThings(combo.one, combo.two, i)} />)
+      }
+    })
+  
+    return blocks;
+  }
+
+  function getNewColors() {
+    const newColors = generateRandomColorsArray(numberOfColors);
+    setColorsArray(newColors);
+  }
+
+  function addNewColor() {
+    setNumberOfColors(numberOfColors + 1);
+  }
 
   return (
-    <>
-    <button onClick={() => setNumberOfColors(numberOfColors+1)}>Click me</button>
-    <Container>
-     {drawRawColors(numberOfColors)}
-    </Container>
-    <Container>
-      {drawMixedBlocks(numberOfColors)}
-    </Container>
+  <>
+    {/* These buttons are experimental and currently break.
+    Also, the raw blocks are not being redrawn. */}
 
+    <button onClick={() => getNewColors()}>New colors</button>
+    <button onClick={() => addNewColor()}>Add color</button>
+
+    <Container>
+      {drawRawBlocks()}
+    </Container>
+    <Container>
+      {drawMixedBlocks()}
+    </Container>
   </>
   )
 }
